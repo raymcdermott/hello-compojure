06e30091cd0cd703c93c3661633985554a529ae7
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-10-25 23:26:36 +0100

    Reading registry to enable connection to Datomic

--- src/hello_compojure/handler.clj
+++ src/hello_compojure/handler.clj
@@ -1,14 +1,15 @@
 (ns hello-compojure.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [hello-compojure.registry :as registry]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 ; in here we are going to connect to the back end Datomic DB and say hello to the only user in the db
 
-(def datomic (get-db))
+(def datomic (registry/db-connect))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (str "Hello World"))
   (route/not-found "Not Found"))
 
 (def app
--- src/hello_compojure/registry.clj
+++ src/hello_compojure/registry.clj
@@ -1,19 +1,29 @@
 (ns hello-compojure.registry
   (:require [taoensso.carmine :as redis :refer (wcar)]
+            [datomic.api :as d]
+            [cheshire.core :refer :all]
             [environ.core :refer [env]]))
 
-(defn get-redis-connection-pool []
+(defn- get-redis-connection-pool []
   (let [spec {:pool {} :spec (if-let [uri (env :redis-url)]
                                {:uri uri}
                                {:host "127.0.0.1" :port 6379})}]
     spec))
 
-(defn set-domain-map-in-redis! [k v]
-  "Stores value v against a key k in Redis. Returns value v"
-  (and k v (redis/wcar (get-redis-connection-pool)
-                       (redis/setex k (or (env :redis-ttl-seconds) 1800) v))
-       v))
+(defn- look-up-datomic [list-name]
+  (if-let [value (redis/wcar (get-redis-connection-pool) (redis/lindex list-name 0))]
+    (parse-string value true)))
 
-(defn get-domain-map-from-redis [k]
-  (if-let [value (redis/wcar (get-redis-connection-pool) (redis/get k))]
-    value))
+
+;(defn set-domain-map-in-redis! [k v]
+;  "Stores value v against a key k in Redis. Returns value v"
+;  (and k v (redis/wcar (get-redis-connection-pool)
+;                       (redis/setex k (or (env :redis-ttl-seconds) 1800) v))
+;       v))
+
+(defn db-connect []
+  (let [datomic (look-up-datomic "datomic")
+        uri (str "datomic:sql://" (:host datomic) ":" (:port datomic) "/customer")
+        conn (d/connect uri)
+        db (d/db conn)]
+    db))
